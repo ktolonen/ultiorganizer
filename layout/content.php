1adf795d8a5a95551bb9dd5a217fbbc5b3f8fbe6
ktolonen <ktolonen@8bdcaf3e-8729-0410-b292-a99d6062519c>
2009-07-19 18:07:15 +0000

    old code
    git-svn-id: https://ultiorganizer.svn.sourceforge.net/svnroot/ultiorganizer/trunk/pelikone3@108 8bdcaf3e-8729-0410-b292-a99d6062519c

--- layout/content.php
+++
@@ -1,7 +0,0 @@
-
-<div class="content">
-<p>
-Mik&auml;li olet joukkueen yhteyshenkil&ouml;, niin anna viereisess&auml; ikkunassa oleviin kenttiin k&auml;ytt&auml;j&auml;tunnuksesi
-ja salasanasi, jotta tied&auml;mme mink&auml; joukkueen edustaja olet.
-</p>
-</div>
--- layout/leftmenu.php
+++
@@ -1,9 +0,0 @@
-<div class="menu_left">
-	
-<?php 
-include $homedir.'\lib\menuBuilder.php';
-buildLeft($LAYOUT_ID);
-?>	
-
-
-</div>
