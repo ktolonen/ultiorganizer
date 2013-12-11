33ac7dac3daa10a79ae61671b46e84361b5f3865
Steffen Mecke <stm.eressea@gmx.net>
2013-12-11 16:24:00 +0100

    minor fix to pool ordering in menu

--- lib/user.functions.php
+++ lib/user.functions.php
@@ -349,8 +349,9 @@ function getViewPools($selSeasonId) {
 
 
 	if ($numselectors > 0) {
-		$query .= ") ORDER BY ser.season ASC, ser.ordering ASC, pool.ordering ASC";
+		$query .= ")";
 	}
+	$query .= " ORDER BY ser.season ASC, ser.ordering ASC, pool.ordering ASC";
 	$result = mysql_query($query);
 	if (!$result) { die('Invalid query: ' . mysql_error()); }
 
