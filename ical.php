7dffeea0662664e7dcd05ab9cb8d5f005c5512d5
kari <kari.m.hulkko@gmail.com>
2026-04-23 10:24:56 +0300

    Improve ical generation to match to the standard

--- ical.php
+++ ical.php
@@ -7,7 +7,70 @@ include_once 'lib/team.functions.php';
 include_once 'lib/location.functions.php';
 include_once 'lib/timetable.functions.php';
 
-header("Content-Type: text/Calendar; charset=utf-8");
+header("Content-Type: text/calendar; charset=utf-8");
+
+function IcalEscapeText($value)
+{
+  $value = trim((string)$value);
+  $value = str_replace("\\", "\\\\", $value);
+  $value = str_replace(array("\r\n", "\r", "\n"), "\\n", $value);
+  $value = str_replace(",", "\\,", $value);
+  $value = str_replace(";", "\\;", $value);
+  return $value;
+}
+
+function IcalFoldLine($line)
+{
+  $limit = 75;
+  if (strlen($line) <= $limit) {
+    return $line;
+  }
+
+  $folded = '';
+  $currentLine = '';
+  $characters = preg_split('//u', $line, -1, PREG_SPLIT_NO_EMPTY);
+  foreach ($characters as $character) {
+    if (strlen($currentLine . $character) > $limit) {
+      $folded .= $currentLine . "\r\n";
+      $currentLine = ' ' . $character;
+    } else {
+      $currentLine .= $character;
+    }
+  }
+
+  return $folded . $currentLine;
+}
+
+function IcalPrintLine($name, $value = null, $params = array())
+{
+  $line = $name;
+  foreach ($params as $paramName => $paramValue) {
+    if ($paramValue !== '') {
+      $line .= ';' . $paramName . '=' . $paramValue;
+    }
+  }
+  if ($value !== null) {
+    $line .= ':' . $value;
+  }
+
+  echo IcalFoldLine($line) . "\r\n";
+}
+
+function IcalDateTimeUtc($timestamp, $timezone = '')
+{
+  if (empty($timestamp)) {
+    return '';
+  }
+
+  try {
+    $dateTimeZone = !empty($timezone) ? new DateTimeZone($timezone) : new DateTimeZone(date_default_timezone_get());
+    $dateTime = new DateTime($timestamp, $dateTimeZone);
+    $dateTime->setTimezone(new DateTimeZone('UTC'));
+    return $dateTime->format('Ymd\THis\Z');
+  } catch (Exception $e) {
+    return gmdate('Ymd\THis\Z', strtotime($timestamp));
+  }
+}
 
 $order = 'tournaments';
 $timefilter = 'coming';
@@ -45,25 +108,40 @@ if (iget("time")) {
 
 $games = TimetableGames($id, $gamefilter, $timefilter, $order);
 
-echo "BEGIN:VCALENDAR\n";
-echo "VERSION:2.0\n";
-echo "PRODID: " . _("Ultiorganizer") . "\n\n";
+IcalPrintLine('BEGIN', 'VCALENDAR');
+IcalPrintLine('VERSION', '2.0');
+IcalPrintLine('PRODID', '-//Ultiorganizer//NONSGML v1.0//EN');
+IcalPrintLine('CALSCALE', 'GREGORIAN');
+IcalPrintLine('METHOD', 'PUBLISH');
 
 foreach ($games as $game) {
   $location = !empty($game['place_id']) ? LocationInfo($game['place_id']) : null;
-  echo "\nBEGIN:VEVENT";
-  echo "\nSUMMARY:" . TeamName($game['hometeam']) . "-" . TeamName($game['visitorteam']);
-  echo "\nDESCRIPTION:" . U_($game['seriesname']) . ": " . U_($game['poolname']);
-  echo "\nLOCATION: " . $game['placename'] . " " . $game['fieldname'];
-  if (!empty($game['timezone'])) {
-    echo "\nDTSTART;TZID=" . $game['timezone'] . ":" . TimeToIcal($game['time']);
+  $homeTeamName = trim(TeamName($game['hometeam']));
+  $visitorTeamName = trim(TeamName($game['visitorteam']));
+  if ($homeTeamName !== '' && $visitorTeamName !== '') {
+    $summary = $homeTeamName . ' vs ' . $visitorTeamName;
+  } elseif ($homeTeamName !== '') {
+    $summary = $homeTeamName;
+  } elseif ($visitorTeamName !== '') {
+    $summary = $visitorTeamName;
   } else {
-    echo "\nDTSTART:" . TimeToIcal($game['time']);
+    $summary = 'TBD';
   }
-  echo "\nDURATION: P" . intval($game['timeslot']) . "M";
+  $description = trim(U_($game['seriesname']) . ': ' . U_($game['poolname']), ': ');
+  $locationName = preg_replace('/\s+/', ' ', trim($game['placename'] . ' ' . $game['fieldname']));
+  $uid = 'game-' . (int)$game['game_id'] . '@ultiorganizer';
+
+  IcalPrintLine('BEGIN', 'VEVENT');
+  IcalPrintLine('UID', $uid);
+  IcalPrintLine('DTSTAMP', gmdate('Ymd\THis\Z'));
+  IcalPrintLine('SUMMARY', IcalEscapeText($summary));
+  IcalPrintLine('DESCRIPTION', IcalEscapeText($description));
+  IcalPrintLine('LOCATION', IcalEscapeText($locationName));
+  IcalPrintLine('DTSTART', IcalDateTimeUtc($game['time'], $game['timezone']));
+  IcalPrintLine('DURATION', 'PT' . intval($game['timeslot']) . 'M');
   if (!empty($location) && isset($location['lat']) && isset($location['lng'])) {
-    echo "\nGEO:" . $location['lat'] . ";" . $location['lng'];
+    IcalPrintLine('GEO', (float)$location['lat'] . ';' . (float)$location['lng']);
   }
-  echo "\nEND:VEVENT\n";
+  IcalPrintLine('END', 'VEVENT');
 }
-echo "\nEND:VCALENDAR\n";
+IcalPrintLine('END', 'VCALENDAR');
