c23952c330323774b2ab44ad18ef5539442b6b74
kari <kari.m.hulkko@gmail.com>
2026-05-09 11:51:56 +0300

    Derive routed views relative to app root

--- lib/view.guard.php
+++ lib/view.guard.php
@@ -15,3 +15,32 @@ function requireRoutedView($view, $indexPath = 'index.php')
   header('Location: ' . $location);
   exit();
 }
+
+function routedViewFromScript($baseDir)
+{
+  $baseDirReal = realpath($baseDir);
+  $scriptFilename = $_SERVER['SCRIPT_FILENAME'] ?? '';
+
+  if ($baseDirReal !== false && $scriptFilename !== '') {
+    $scriptReal = realpath($scriptFilename);
+    $basePrefix = rtrim($baseDirReal, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+
+    if ($scriptReal !== false && strpos($scriptReal, $basePrefix) === 0) {
+      $view = substr($scriptReal, strlen($basePrefix));
+      $view = str_replace(DIRECTORY_SEPARATOR, '/', $view);
+      return preg_replace('/\.php$/i', '', $view);
+    }
+  }
+
+  $scriptName = isset($_SERVER['SCRIPT_NAME']) ? (string)$_SERVER['SCRIPT_NAME'] : '';
+  $view = ltrim(preg_replace('/\.php$/i', '', $scriptName), '/');
+  $parts = explode('/', $view);
+  foreach (array('admin', 'user') as $root) {
+    $offset = array_search($root, $parts, true);
+    if ($offset !== false) {
+      return implode('/', array_slice($parts, $offset));
+    }
+  }
+
+  return $view;
+}
--- user/auth.php
+++ user/auth.php
@@ -5,8 +5,7 @@ if (!isset($include_prefix)) {
 
 include_once $include_prefix . 'lib/view.guard.php';
 if (!defined('UO_ROUTED_VIEW')) {
-	$scriptName = isset($_SERVER['SCRIPT_NAME']) ? (string)$_SERVER['SCRIPT_NAME'] : '';
-	$view = ltrim(preg_replace('/\.php$/i', '', $scriptName), '/');
+	$view = routedViewFromScript($include_prefix);
 	if ($view !== '') {
 		requireRoutedView($view, '../index.php');
 	}
